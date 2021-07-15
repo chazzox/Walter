8ec321ee588b79d3a64851ee2f4abcc6af688ef1
chazzox <caylott@outlook.com>
2021-07-15 22:58:39 +0100

    Added boolean for detecting what state the gui should be in

--- Walter.cpp
+++ Walter.cpp
@@ -2,17 +2,27 @@
 #include <iostream>
 
 int main() {
-    if (RegisterHotKey(NULL,1,MOD_ALT,0x42)) {
-        std::cout << "registered the hotkey correctly" << std::endl;
-    }
+	bool is_open = false;
+	// hot key is on alt+b
+	if (RegisterHotKey(NULL,1,MOD_ALT,0x42)) {
+       		std::cout << "registered the hotkey correctly" << std::endl;
+    	}
 
-    MSG msg = { 0 };
+    	MSG msg = { 0 };
 
-    while (GetMessage(&msg, NULL, 0, 0) != 0) {
-        if (msg.message == WM_HOTKEY)
-        {
-            std::cout << "WM_HOTKEY received" << std::endl;
-        }
-    }
-    return 0;
+    	while (GetMessage(&msg, NULL, 0, 0) != 0) {
+	        if (msg.message == WM_HOTKEY)
+       		{
+	            std::cout << "WM_HOTKEY received" << std::endl;
+		    is_open = !is_open;
+		    if(is_open){
+			    std::cout << "open gui" << std::endl;
+		    }
+		    else{
+			    std::cout << "close gui" << std::endl;
+			}
+       		}
+    	}
+   	 return 0;
 }
+
